de7ac20ec6954b02b1ab200d23cfb78425416da3
codingmasti <adityamaurya1138@gmail.com>
2026-06-27 18:00:51 +0530

    Fix build error

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
-import { property } from "three/tsl";
 
 export const DataContext = createContext(null);
 
